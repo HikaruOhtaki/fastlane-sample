8a75b6c7939f9b500a704733ca6a6e8400db1583
Hikaru.Ohtaki <hikaru.11.oth@icloud.com>
2021-12-17 01:50:27 +0900

    Initial Commit

---
+++ fastlane-sample/fastlane_sampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  fastlane_sampleApp.swift
+//  fastlane-sample
+//
+//  Created by ohtaki hikaru on 2021/12/17.
+//
+
+import SwiftUI
+
+@main
+struct fastlane_sampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
